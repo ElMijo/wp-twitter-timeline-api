737d9d4b2c364d5b40327990109ec7fbf6690f1f
Jerry Anselmi <janselmi@mppi.gob.ve>
2015-03-10 15:57:14 -0430

    agregando la clase que consumira el servicio del API de Twitter y otras cosillas mas

--- src/WTwitterTimelineAPI/WTwitterTimelineAPI.php
+++ src/WTwitterTimelineAPI/WTwitterTimelineAPI.php
@@ -14,19 +14,28 @@
 */
 class WTwitterTimelineAPI   extends WTwitterTimelineAPISettingsFactory  implements WTwitterTimelineAPISettingsInterface
 {
+    /**
+     * slug del plugin
+     * @var string
+     */
+    protected $menu_slug = 'wtwitter-timeline-api';
+
     function __construct()
     {
+        parent::__construct();
         add_action( 'admin_menu', array($this,'register_menu'));
     }
 
     final public function register_menu()
     {
-        add_menu_page('Twitter Timeline API', 'WTwitter', 'manage_options', 'wtwitter-timeline-api', array($this,'menu_page'), WTTAPI_URL.'/images/icon.png',60);
+        add_menu_page('Twitter Timeline API', 'WTwitter', 'manage_options', $this->menu_slug, array($this,'menu_page'), WTTAPI_URL.'/images/icon.png',60);
     }
 
     final public function menu_page()
     {
+        $titulo = get_admin_page_title();
 
+        include WTTAPI_DIR.'/inc/template/options.phtml';
     }
     /**
      * @see WTwitterTimelineAPISettingsInterface::get_settings
@@ -34,34 +43,65 @@ class WTwitterTimelineAPI   extends WTwitterTimelineAPISettingsFactory  implemen
     public function get_settings()
     {
         return array(
-            // 'estilo' => array(
-            //     array(
-            //         'id'    => 'color-base',
-            //         'title' => __('Color Base',WPACE_DOMAIN),
-            //         'type'  => 'color'
-            //     ),
-            //     array(
-            //         'id'    => 'tema-pace',
-            //         'title' => __('Tema',WPACE_DOMAIN),
-            //         'type'  => 'select',
-            //         'list'  => array(
-            //             'Minimal',
-            //             'Flash',
-            //             'Barber Shop',
-            //             'Mac OSX',
-            //             'Fill Left',
-            //             'Flat Top',
-            //             'Big Counter',
-            //             'Corner Indicator',
-            //             'Bounce',
-            //             'Loading Bar',
-            //             'Center Circle',
-            //             'Center Atom',
-            //             'Center Radar',
-            //             'Center Simple'
-            //         )
-            //     ),
-            // )
+            'default' => array(
+                array(
+                    'id' => 'twitter-username',
+                    'title' => 'Username',
+                    'type' => 'text',
+                    'help' => __('Nombre de usuario',WTTAPI_DOMAIN),
+                    'attrs' => array(
+                        'class' => 'regular-text'
+                    )
+                ),
+                array(
+                    'id' => 'twitter-number',
+                    'title' => 'N° Tweets',
+                    'type' => 'number',
+                    'help' => __('Cantidad de tweets que se requieren por consulta',WTTAPI_DOMAIN),
+                    'attrs' => array(
+                        'class' => 'small-text'
+                    )
+
+                )
+            ),
+            'auth' => array(
+                array(
+                    'id' => 'twitter-consumer-key',
+                    'title' => 'Consumer Key',
+                    'type' => 'text',
+                    'help' => __('Llave generada por el API de twitter para consumir los servicios',WTTAPI_DOMAIN),
+                    'attrs' => array(
+                        'class' => 'large-text'
+                    )
+                ),
+                array(
+                    'id' => 'twitter-consumer-secret',
+                    'title' => 'Consumer Secret',
+                    'type' => 'text',
+                    'help' => __('Llave secreta generada por el API de twitter para consumir los servicios',WTTAPI_DOMAIN),
+                    'attrs' => array(
+                        'class' => 'large-text'
+                    )
+                ),
+                array(
+                    'id' => 'twitter-access-token',
+                    'title' => 'Access Token',
+                    'type' => 'text',
+                    'help' => __('Token de acceso generada por el API de twitter',WTTAPI_DOMAIN),
+                    'attrs' => array(
+                        'class' => 'large-text'
+                    )
+                ),
+                array(
+                    'id' => 'twitter-access-token-secret',
+                    'title' => 'Access Token Secret',
+                    'type' => 'text',
+                    'help' => __('Token secreto de acceso generada por el API de twitter',WTTAPI_DOMAIN),
+                    'attrs' => array(
+                        'class' => 'large-text'
+                    )
+                ),
+            )
         );
     }
 }
